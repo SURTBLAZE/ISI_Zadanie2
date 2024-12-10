e48bb0bc0e112e138e1024b0660de159a69b777e
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-12-10 21:31:37 +0100

    change program design

--- GUI.py
+++ GUI.py
@@ -1,53 +1,98 @@
+import time
 import tkinter as tk
 from tkinter import messagebox
-import time
-from Field import Field
+
 from AIPlayer import AIPlayer
+from Field import Field
 
 
 class GUI:
     def __init__(self, field: Field):
         self.field = field
         self.player = AIPlayer(field)
+        self.is_stopped = False
+        self.is_algorithm_running = False
 
     def run(self):
         root = tk.Tk()
         root.title("Lloydova 8-ička (15-tka)")
 
-        canvas = tk.Canvas(root, width=300, height=300, bg="white")
-        canvas.pack(pady=10)
+        frame = tk.Frame(root)
+        frame.pack(padx=30, pady=(30, 40))
+
+        canvas = tk.Canvas(frame, bg="#CD7F32")
+        canvas.pack()
 
         button_frame = tk.Frame(root)
-        button_frame.pack(pady=10)
+        button_frame.pack(pady=(0, 30))
 
-        tk.Button(button_frame, text="DFS", command=lambda: self.on_solve("DFS", canvas)).grid(row=0, column=0, padx=5)
-        tk.Button(button_frame, text="Astar", command=lambda: self.on_solve("Astar", canvas)).grid(row=0, column=1, padx=5)
-        tk.Button(button_frame, text="GreedySearch", command=lambda: self.on_solve("GreedySearch", canvas)).grid(row=0,
-                                                                                                            column=2,
-                                                                                                            padx=5)
+        button_frame.grid_columnconfigure(2, minsize=50)
+
+        (tk.Button(button_frame,
+                   text="Reset", bg="green", fg="white", padx=7, pady=3,
+                   command=lambda: self.reset_grid(canvas))
+         .grid(row=0, column=0, padx=5))
+
+        (tk.Button(button_frame,
+                   text="Stop", bg="red", fg="white", padx=7, pady=3,
+                   command=lambda: self.stop_algorithm())
+         .grid(row=0, column=1, padx=5))
+
+        (tk.Button(button_frame, text="DFS", padx=7, pady=3, bg="#CBCBCB",
+                   command=lambda: self.on_solve("DFS", canvas))
+         .grid(row=0, column=3, padx=5))
+
+        (tk.Button(button_frame, text="Astar", padx=7, pady=3, bg="#CBCBCB",
+                   command=lambda: self.on_solve("Astar", canvas))
+         .grid(row=0, column=4, padx=5))
+
+        (tk.Button(button_frame, text="GreedySearch", padx=7, pady=3, bg="#CBCBCB",
+                   command=lambda: self.on_solve("GreedySearch", canvas))
+         .grid(row=0, column=5, padx=5))
 
         self.draw_grid(canvas, self.field.get_field())
 
         root.mainloop()
 
-    def draw_grid(self,canvas, state):
+    def stop_algorithm(self):
+        if self.is_algorithm_running:
+            self.is_stopped = True
+            self.is_algorithm_running = False
+
+    def reset_grid(self, canvas):
+        if not self.is_algorithm_running:
+            self.draw_grid(canvas, self.field.get_field())
+
+    def draw_grid(self, canvas, state):
         canvas.delete("all")
         rows, cols = len(state), len(state[0])
-        cell_size = 100
+        cell_size = 150
+        frame_padding = 20
 
-
-        canvas.config(width=cols * cell_size, height=rows * cell_size)
+        canvas.config(width=cols * cell_size + 2 * frame_padding,
+                      height=rows * cell_size + 2 * frame_padding)
 
         for i in range(rows):
             for j in range(cols):
-                x1, y1 = j * cell_size, i * cell_size
+                x1, y1 = j * cell_size + frame_padding, i * cell_size + frame_padding
                 x2, y2 = x1 + cell_size, y1 + cell_size
                 value = state[i][j]
                 if value != 0:
-                    canvas.create_rectangle(x1, y1, x2, y2, fill="lightblue", outline="black")
-                    canvas.create_text((x1 + x2) // 2, (y1 + y2) // 2, text=str(value), font=("Arial", 24))
+                    canvas.create_rectangle(
+                        x1, y1, x2, y2,
+                        fill="#FFFDD0", outline="black"
+                    )
+                    canvas.create_text(
+                        (x1 + x2) // 2, (y1 + y2) // 2,
+                        text=str(value), font=("Arial", 24), fill="darkblue"
+                    )
+                else:
+                    canvas.create_rectangle(x1, y1, x2, y2, fill="#FFF8F1", outline="black")
 
     def on_solve(self, method, canvas):
+        if self.is_algorithm_running:
+            return
+
         path = self.solve_puzzle(method)
         if not path:
             messagebox.showerror("Error", f"no solution from {method}")
@@ -59,9 +104,17 @@ class GUI:
 
     def animate_solution(self, canvas, path, speed):
         for state in path:
+            if self.is_stopped:
+                self.is_stopped = False
+                return
+
+            self.is_algorithm_running = True
+
             self.draw_grid(canvas, state.get_field())
             canvas.update()
             time.sleep(1 / speed)
+
+        self.is_algorithm_running = False
 
     def solve_puzzle(self, method):
         if method == "DFS":
